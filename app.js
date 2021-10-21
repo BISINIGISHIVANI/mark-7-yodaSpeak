7f8445d1efe46c0aa7881e062f56d6d5bbc5bad5
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2021-10-21 22:59:03 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var textInput =document.querySelector("#text-area");
+var btnTranslate=document.querySelector("#translate-button");
+var textOutput=document.querySelector("#out-put");
+
+var serverURL=""
+
+btnTranslate.addEventListener("click",clickHandler);
+
+function clickHandler(){
+
+}
